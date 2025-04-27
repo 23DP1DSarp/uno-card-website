264317e00cce59163aa2358a4287f17dd590bf3e
23DP1DSarp <A230246DS@rvt.lv>
2025-04-27 16:46:17 +0300

    server.js fix

--- src/js/server.js
+++ src/js/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://23dp1dsarp.github.io/uno-card-website/'
+  origin: 'https://23dp1dsarp.github.io'
 }));
 app.use(bodyParser.json());
 
